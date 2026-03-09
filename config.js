0b0d41cb02ae5b2efadbb219888b029ae3b5951a
tysbeatz <99852682+Tysbeatz@users.noreply.github.com>
2026-03-09 13:17:28 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,62 @@
+const config = {
+
+name:"Sachin Kumar",
+
+logo:"Sachin.dev",
+
+title:"Frontend Developer | Java Programmer",
+
+profile:"assets/profile.jpg",
+
+about:"I am a passionate developer who loves building web applications and learning new technologies.",
+
+skills:[
+"HTML",
+"CSS",
+"JavaScript",
+"Java",
+"SQL"
+],
+
+projects:[
+
+{
+title:"Portfolio Website",
+desc:"My personal developer portfolio",
+link:"#"
+},
+
+{
+title:"Weather App",
+desc:"Weather app using API",
+link:"#"
+},
+
+{
+title:"Todo App",
+desc:"Task manager using JavaScript",
+link:"#"
+}
+
+],
+
+social:[
+
+{
+name:"GitHub",
+link:"https://github.com"
+},
+
+{
+name:"LinkedIn",
+link:"#"
+},
+
+{
+name:"Email",
+link:"mailto:test@gmail.com"
+}
+
+]
+
+}
